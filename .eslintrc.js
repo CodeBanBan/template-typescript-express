4b04d27c4f8f605dd3a48fc69a15ee3dc9475754
Tatthep Chayaphiwat <tatthep.c@hoteliers.guru>
2025-05-02 11:29:54 +0700

    chore(lint): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['standard-with-typescript'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  env: {
+    mocha: true
+  },
+  rules: {
+    // 'object-shorthand': 'off'
+  }
+}
