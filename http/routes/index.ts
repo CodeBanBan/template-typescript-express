eef9e5b929263dbd8aea8633c8f1129dab3a10a1
CodeBanBan <>
2025-06-16 16:59:25 +0700

    feat(redis): add sample http controller

--- http/routes/index.ts
+++ http/routes/index.ts
@@ -1,8 +1,10 @@
 import Express from 'express'
 import * as HelloRoute from './hello-route'
+import * as RedisRoute from './redis-route'
 
 const _router = Express()
 
 _router.use('/hello', HelloRoute.router)
+_router.use('/redis', RedisRoute.router)
 
 export const router = _router
